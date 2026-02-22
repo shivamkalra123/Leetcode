429c01b2213858c9ebe0df986f08291c179c3938
Shivam Kalra <60731601+shivamkalra123@users.noreply.github.com>
2026-02-22 12:09:27 +0530

    11 ms (25.09%) 24.9 MB (10.18%) 02-22-2026

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,18 +1,17 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        nums.sort()
-        a={}
-        b=[]
-        for x in nums:
-            if x in a:
-                a[x]+=1
-            else:
-                a[x]=1
-            if a[x]>=k:
-                b.append(x)
-        return list(set(b))
-        
-        
-        
-            
+        hashmap={}
+        res=[[] for i in range(len(nums)+1)]
+        for j in nums:
+            hashmap[j]=1+hashmap.get(j,0)
+        for i,e in hashmap.items():
+            res[e].append(i)
+        output=[]
+        for i in range(len(res)-1,0,-1):
+            for num in res[i]:
+                output.append(num)
+                if len(output)==k:
+                    return output
+
+
         
